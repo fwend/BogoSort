472307b59c8c3123b1ff85a64ce0e5089250df8a
Jos van Uden <josvanuden@gmail.com>
2019-02-19 21:48:32 +0100

    Add main file

---
+++ bogosort.d
@@ -0,0 +1,17 @@
+import std.stdio, std.random, std.algorithm;
+
+void main() {
+    auto a = [4,3,8,9,1,7,2,5,6];
+	writeln(a);
+    bogoSort(a);
+    writeln(a);      
+}
+
+int bogoSort(int[] a) {
+    int count;
+    do {
+        randomShuffle(a);
+        count++;
+    } while (!isSorted(a));
+    return count;
+}
